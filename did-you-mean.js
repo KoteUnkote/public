d9a7bfaaa12545ea99b5e35c35b763bc93d676a5
Wanda Carlson <87515018+wanda-carlson@users.noreply.github.com>
2023-12-05 16:37:37 +0500

    Update did-you-mean.js

--- did-you-mean.js
+++ did-you-mean.js
@@ -9,7 +9,17 @@
       const readJson = require('read-package-json-fast')
       const { commands } = require('./cmd-list.js')
       // ... (your existing code)
+
+
+      const { distance } = require('fastest-levenshtein')
+      const readJson = require('read-package-json-fast')
+      const { commands } = require('./cmd-list.js')
+      // ... (your existing code)
       module.exports = didYouMean
+//testing automation script
+    
+
+
 //testing automation script
     
 
